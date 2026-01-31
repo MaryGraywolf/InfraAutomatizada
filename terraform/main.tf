cd95f57a4132d17a90de94929e0b01c08c2d5a98
Mary Santos <marysantoslima24@gmail.com>
2026-01-31 10:53:41 -0300

    Configuração da vpc e gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,44 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws"{
     region = "us-east-1"
 }
 
-resource "aws_security_group" "web_sg" {
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    owners = ["099720109477"]
+
+    filter {
+        name = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    }
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+    tags = {
+      name = "Rede-Loucuras"
+    }
+}
+
+resource "aws_internet_gateway" "g_main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    name = "gw-rede-loucuras"
+  }
+}
+
+resource "aws_security_group" "webserver" {
     name = "sg_web_nginx"
     description = "Permite acesso HTTP e SSH"
 
@@ -25,16 +61,6 @@ resource "aws_security_group" "web_sg" {
         to_port     = 0
         protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
-data "aws_ami" "ubuntu" {
-    most_recent = true
-    owners = ["099720109477"]
-
-    filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
     }
 }
 
